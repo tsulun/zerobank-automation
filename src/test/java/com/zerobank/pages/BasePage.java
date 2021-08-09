c045433b55df7b0b0cb5df7443b8341e7562cdcd
tsulun <tsulun16@gmail.com>
2021-08-09 01:21:53 +0100

    updated PayBillsStepDefs class and basePage

--- src/test/java/com/zerobank/pages/BasePage.java
+++ src/test/java/com/zerobank/pages/BasePage.java
@@ -5,17 +5,24 @@ import com.zerobank.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import java.util.concurrent.BrokenBarrierException;
 
 public class BasePage {
-
+    public BasePage () {
+        PageFactory.initElements(Driver.get(), this);
+    }
 
 public void navigateToModule(String Tab){
 
-    WebElement tabElement = Driver.get().findElement(By.linkText(Tab));
-    BrowserUtils.waitForClickablility(tabElement , 5);
-    tabElement.click();
+    try {
+        BrowserUtils.waitForClickablility(By.linkText(Tab), 5);
+        WebElement tabElement = Driver.get().findElement(By.linkText(Tab));
+        tabElement.click();
+    }catch (Exception e) {
+        BrowserUtils.clickWithWait(By.linkText(Tab), 5);
+    }
 }
 
 
--- src/test/java/com/zerobank/stepdefinitions/PayBillsStepDefs.java
+++ src/test/java/com/zerobank/stepdefinitions/PayBillsStepDefs.java
@@ -3,37 +3,37 @@ package com.zerobank.stepdefinitions;
 import com.zerobank.pages.BasePage;
 import com.zerobank.pages.PayBillsPage;
 import com.zerobank.utilities.BrowserUtils;
+import com.zerobank.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.JavascriptExecutor;
+
+import java.util.Map;
 
 public class PayBillsStepDefs {
     @Given("Add New Payeetab")
     public void add_New_Payeetab() {
         PayBillsPage payBillsPage = new PayBillsPage();
-
         payBillsPage.addNewPayee.click();
     }
 
-    @Given("creates new payee usingfollowing information")
-    public void creates_new_payee_usingfollowing_information(io.cucumber.datatable.DataTable dataTable) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
-        //
-        // For other transformations you can register a DataTableType.
-        throw new io.cucumber.java.PendingException();
+    @Given("creates new payee using following information")
+    public void creates_new_payee_using_following_information(Map<String,String > userInfo) {
+        PayBillsPage payBillsPage = new PayBillsPage();
+        payBillsPage.payeeName.sendKeys(userInfo.get("Payee Name"));
+        payBillsPage.payeeAddress.sendKeys(userInfo.get("Payee Address"));
+        payBillsPage.account.sendKeys(userInfo.get("Account"));
+        payBillsPage.payeeDetails.sendKeys(userInfo.get("Payee details"));
+        payBillsPage.add.click();
     }
 
     @Then("message The new payee The Law Offices of Hyde, Price & Scharks was successfully created")
     public void message_The_new_payee_The_Law_Offices_of_Hyde_Price_Scharks_was_successfully_created() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Assert.assertTrue(new PayBillsPage().succesfulMessage.isDisplayed());
     }
 
 
 
-    Process finished with exit code 0
 
 }
