ab8a092bff17abe6104b103210f6c1ac9b29e2ba
tsulun <tsulun16@gmail.com>
2021-08-09 02:07:52 +0100

    cukes

--- src/test/java/com/zerobank/runners/CukesRunner.java
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
                 "html:target/default-html-reports"},
         features= "src/test/resources/features",
         glue = "com/zerobank/stepdefinitions",
-        dryRun = true,
+        dryRun = false,
         tags = "@wip"
 
 )
