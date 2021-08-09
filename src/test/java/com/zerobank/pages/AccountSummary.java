fa70e1d6f09c02d35495680bc6b763c480659f99
tsulun <tsulun16@gmail.com>
2021-08-10 00:55:32 +0100

    added AccountActivity and AccountSummary pages

---
+++ src/test/java/com/zerobank/pages/AccountSummary.java
@@ -0,0 +1,18 @@
+package com.zerobank.pages;
+
+import com.zerobank.utilities.Driver;
+import com.zerobank.utilities.StringUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class AccountSummary extends BasePage{
+
+    public void clickAccount(String str){
+        WebElement webElement = Driver.get().findElement(By.xpath("//a[text()='"+ new StringUtils().capitalizeWord(str)  +"']"));
+        webElement.click();
+
+    }
+
+
+
+}
