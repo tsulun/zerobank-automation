2a63784851ea93c5911934511338e11c69698db7
tsulun <tsulun16@gmail.com>
2021-08-10 19:01:06 +0100

    correction for @wip

--- src/test/java/com/zerobank/runners/CukesRunner.java
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         features= "src/test/resources/features",
         glue = "com/zerobank/stepdefinitions",
         dryRun = false,
-        tags = "@tedros"
+        tags = "@wip"
 
 )
 public class CukesRunner {
