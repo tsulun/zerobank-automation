4af0b96f6eff1f15439220ba7e96d9aef33547e7
Tedros <tedros.tse.ghe@gmail.com>
2021-08-17 22:06:30 +0100

    new methods on the FindTransactionsPage and changes on the stepdefs and the features

--- src/test/java/com/zerobank/pages/FindTransactionsPage.java
+++ src/test/java/com/zerobank/pages/FindTransactionsPage.java
@@ -1,16 +1,20 @@
 package com.zerobank.pages;
 
 import com.zerobank.utilities.Driver;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
-public class FindTransactionsPage extends BasePage{
-
+public class FindTransactionsPage extends BasePage {
+    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
     @FindBy(xpath = "//a[contains(text(),'Find Transactions')]")
     public WebElement findTransctionTab;
@@ -33,20 +37,20 @@ public class FindTransactionsPage extends BasePage{
     @FindBy(xpath = "//div[@id='filtered_transactions_for_account']//tbody/tr/td[2]")
     public List<WebElement> secondColumn;
 
-    public WebElement datesFromTo(int i){
+    public WebElement datesFromTo(int i) {
         return Driver.get().findElement(By.xpath("//div[@id='filtered_transactions_for_account']//tbody/tr[" + i + "]/td[1]"));
     }
 
-    public WebElement resultContains(int i){
-        return Driver.get().findElement(By.xpath("//div[@id='filtered_transactions_for_account']//tbody/tr["+i+"]/td[2]"));
+    public WebElement resultContains(int i) {
+        return Driver.get().findElement(By.xpath("//div[@id='filtered_transactions_for_account']//tbody/tr[" + i + "]/td[2]"));
     }
 
-    public WebElement resultDeposit(int i){
-        return Driver.get().findElement(By.xpath("//div[@id='filtered_transactions_for_account']//tbody/tr["+i+"]/td[3]"));
+    public WebElement resultDeposit(int i) {
+        return Driver.get().findElement(By.xpath("//div[@id='filtered_transactions_for_account']//tbody/tr[" + i + "]/td[3]"));
     }
 
-    public WebElement resultWithdrawal(int i){
-        return Driver.get().findElement(By.xpath("//div[@id='filtered_transactions_for_account']//tbody/tr["+i+"]/td[4]"));
+    public WebElement resultWithdrawal(int i) {
+        return Driver.get().findElement(By.xpath("//div[@id='filtered_transactions_for_account']//tbody/tr[" + i + "]/td[4]"));
     }
 
 
@@ -56,23 +60,141 @@ public class FindTransactionsPage extends BasePage{
     @FindBy(xpath = "//select[@id='aa_type']")
     public WebElement typeOptions;
 
-    public Select typeOptionsList(){
-        return new Select(typeOptions);
+    public void selectOption(String str) {
+        Select selectedDropDown = new Select(typeOptions);
+        selectedDropDown.selectByVisibleText(str);  //select from the dropdown by text
+        selectedDropDown.getFirstSelectedOption(); //the selected drop down is set as the first option to be clicked
+
     }
 
-    public void enteringDateRangeMethod(String from, String to){
+    public void enteringDateRangeMethod(String from, String to) {
         fromDate.sendKeys(from);
         toDate.sendKeys(to);
     }
 
-    public void clearFromDateField(){
+    public void clearFromDateField() {
         fromDate.clear();
     }
-    public void clearToDateField(){
+
+    public void clearToDateField() {
         toDate.clear();
     }
-    public void clearDescriptionField(){
+
+    public void clearDescriptionField() {
         descritptionField.clear();
+    }
+
+    public void inBetweenDatesAssertion(String dateF, String dateT) throws ParseException {
+        //changing to date format instead of string format
+        Date dateFF = format.parse(dateF);
+        Date dateTT = format.parse(dateT);
+        //finding the number of rows
+        int sizeOfRows = firstColumn.size();
+        for (int i = 1; i <= sizeOfRows; i++) {
+            Date dates = format.parse(datesFromTo(i).getText());
+            //dateTT.after(dates) the same as dateTT > dates and dateFF.before(dates) same as dateFF < dates
+            Assert.assertTrue(dateTT.after(dates) && dateFF.before(dates) || dateTT.equals(dates) || dateFF.equals(dates));
+
+        }
+    }
+
+
+    public void descendingSortedOrderChecker() throws ParseException {
+        //finding the row numbers
+        int sizeOfRows = firstColumn.size();
+        // "<sizeOfRows because  datesFromTo(i+1) is i+1 the last one already taken
+        for (int i = 1; i < sizeOfRows; i++) {
+            Date date1 = format.parse(datesFromTo(i).getText()); //first value from the list
+            Date date2 = format.parse(datesFromTo(i + 1).getText()); //second value from the list
+            Assert.assertTrue(date1.after(date2)); //check the most recent date is above the later date
+
+        }
+    }
+
+    public void notContaningTheGivenTransactionDate(String givenDate) throws ParseException {
+        int sizeOfRows = firstColumn.size(); //number of rows
+        Date date1 = format.parse(givenDate);
+        for (int i = 1; i <= sizeOfRows; i++) {
+            //check for each the dates that the given date is not equal to the number rows dates.
+            Date date2 = format.parse(datesFromTo(i).getText());
+            Assert.assertNotEquals(date1, date2);
+        }
+    }
+
+    public void searchResultsContainsTheGivenTextChecker(String str) {
+        int sizeOfRows2 = secondColumn.size(); //number of rows
+        if (sizeOfRows2 > 0) {
+            for (int i = 1; i <= sizeOfRows2; i++) {
+                //fetch the text from the rows
+                String textContainingStr = resultContains(i).getText();
+                Assert.assertTrue(textContainingStr.contains(str));
+            }
+        } else {
+            String textContainingStr = noResults.getText();
+            //the assertion is for case insensitivity but the case is not case insensitive, hence it fails for
+            // for lower case strings.
+            Assert.assertTrue("verify it is case insensitive", textContainingStr.contains(str));
+        }
+    }
+
+
+    public void searchResultShouldNotContainTheGivenTextChecker(String str) {
+        int sizeOfRows2 = secondColumn.size();
+
+        for (int i = 1; i <= sizeOfRows2; i++) {
+            //fetch the text from the rows
+            String textContainingStr = resultContains(i).getText();
+            //asserting that the text is not contained is false , (it is show the text on the table)=> False + ! = true
+            Assert.assertFalse(!textContainingStr.contains(str));
+        }
+    }
+
+    public void valueCheckerUnderGivenColumnName(String str) {
+        int sizeOfRows2 = secondColumn.size();
+        boolean fa = false;
+        if (str.equals("Deposit")) {
+            for (int i = 1; i <= sizeOfRows2; i++) {
+                //if it finds one under deposit the boolean becomes true and it breaks no need to continue the loop
+                // if any idea on shorting this part part or other its most welcome guys.
+                if (!resultDeposit(i).getText().isEmpty()) {
+                    fa = true;
+                    break;
+                }
+            }
+            Assert.assertTrue(fa);
+        } else if (str.equals("Withdrawal")) {
+            for (int i = 1; i <= sizeOfRows2; i++) {
+                //if it finds one under deposit the boolean becomes true and it breaks no need to continue the loop
+                // if any idea on shorting this part part or other its most welcome guys.
+                if (!resultWithdrawal(i).getText().isEmpty()) {
+                    fa = true;
+                    break;
+                }
+            }
+            Assert.assertTrue(fa);
+        }
+    }
+
+    public void noResultUnderGivenColumnName(String str) {
+        int sizeOfRows2 = secondColumn.size();
+        if (str.equals("Withdrawal")) {
+            for (int i = 1; i <= sizeOfRows2; i++) {
+                //if it is empty it good according to the requirement
+                //else if it finds any in the number of rows, it will become false and fail, and
+                //Will it automatically breaks the loop if it fails to satisfy the condition?
+                Assert.assertTrue(resultWithdrawal(i).getText().isEmpty());
+
+            }
+        } else if (str.equals("Deposit")) {
+            for (int i = 1; i <= sizeOfRows2; i++) {
+                //if it is empty it good according to the requirement
+                //else if it finds any in the number of rows, it will become false and fail, and
+                //Will it automatically breaks the loop if it fails to satisfy the condition?
+                Assert.assertTrue(resultDeposit(i).getText().isEmpty());
+            }
+        }
+
+
     }
 
 }
--- src/test/java/com/zerobank/stepdefinitions/FindTransactionsStepDefs.java
+++ src/test/java/com/zerobank/stepdefinitions/FindTransactionsStepDefs.java
@@ -17,7 +17,7 @@ import java.util.Date;
 public class FindTransactionsStepDefs {
 
     FindTransactionsPage findTransactionsPage = new FindTransactionsPage();
-    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
 
     @Given("the user accesses the Find Transactions tab")
     public void the_user_accesses_the_Find_Transactions_tab() {
@@ -42,42 +42,18 @@ public class FindTransactionsStepDefs {
 
     @Then("results table should only show transactions dates between {string} to {string}")
     public void results_table_should_only_show_transactions_dates_between_to(String dateF, String dateT) throws ParseException {
-        //changing to date format instead of string format
-        Date dateFF = format.parse(dateF);
-        Date dateTT = format.parse(dateT);
-        //finding the number of rows
-        int sizeOfRows = findTransactionsPage.firstColumn.size();
-        for (int i = 1; i <= sizeOfRows; i++) {
-            Date dates = format.parse(findTransactionsPage.datesFromTo(i).getText());
-            //dateTT.after(dates) the same as dateTT > dates and dateFF.before(dates) same as dateFF < dates
-            Assert.assertTrue(dateTT.after(dates) && dateFF.before(dates) || dateTT.equals(dates) || dateFF.equals(dates) );
-
-        }
+        findTransactionsPage.inBetweenDatesAssertion(dateF,dateT);
 
     }
 
     @Then("the results should be sorted by most recent date")
     public void the_results_should_be_sorted_by_most_recent_date() throws ParseException { //ParseException it is autmatically generated as the same as like Thread.sleep()
-        //finding the row numbers
-        int sizeOfRows = findTransactionsPage.firstColumn.size();
-        // "<sizeOfRows because  datesFromTo(i+1) is i+1 the last one already taken
-        for (int i = 1; i < sizeOfRows; i++) {
-            Date date1 = format.parse(findTransactionsPage.datesFromTo(i).getText()); //first value from the list
-            Date date2 = format.parse(findTransactionsPage.datesFromTo(i+1).getText()); //second value from the list
-            Assert.assertTrue(date1.after(date2)); //check the most recent date is above the later date
-
-        }
+        findTransactionsPage.descendingSortedOrderChecker();
     }
 
     @Then("the results table should only not contain transactions dated {string}")
-    public void the_results_table_should_only_not_contain_transactions_dated(String date11) throws ParseException {
-        int sizeOfRows = findTransactionsPage.firstColumn.size(); //number of rows
-        Date date1 = format.parse(date11);
-        for (int i = 1; i <= sizeOfRows; i++) {
-            //check for each the dates that the given date is not equal to the number rows dates.
-            Date date2 = format.parse(findTransactionsPage.datesFromTo(i).getText());
-            Assert.assertNotEquals(date1, date2);
-        }
+    public void the_results_table_should_only_not_contain_transactions_dated(String date) throws ParseException {
+       findTransactionsPage.notContaningTheGivenTransactionDate(date);
     }
 
     //the user enters description "ONLINE"
@@ -91,100 +67,33 @@ public class FindTransactionsStepDefs {
     //Then results table should only show descriptions that containing "OFFICE"
     @Then("results table should only show descriptions that containing {string}")
     public void results_table_should_only_show_descriptions_that_containing(String str) {
-        int sizeOfRows2 = findTransactionsPage.secondColumn.size(); //number of rows
-        if(sizeOfRows2>0) {
-            for (int i = 1; i <= sizeOfRows2; i++) {
-                //fetch the text from the rows
-                String textContainingStr = findTransactionsPage.resultContains(i).getText();
-                Assert.assertTrue(textContainingStr.contains(str));
-            }
-        }else {
-                String textContainingStr = findTransactionsPage.noResults.getText();
-                //the assertion is for case insensitivity but the case is not case insensitive, hence it fails for
-                // for lower case strings.
-                Assert.assertTrue("verify it is case insensitive",textContainingStr.contains(str));
-            }
+        findTransactionsPage.searchResultsContainsTheGivenTextChecker(str);
         }
 
 
     //But results table should not show descriptions containing "OFFICE"
     @Then("results table should not show descriptions containing {string}")
     public void results_table_should_not_show_descriptions_containing(String str) {
-        int sizeOfRows2 = findTransactionsPage.secondColumn.size();
         the_user_enters_description(str);
-        for (int i = 1; i <= sizeOfRows2; i++) {
-            //fetch the text from the rows
-            String textContainingStr = findTransactionsPage.resultContains(i).getText();
-            //asserting that the text is not contained is false , (it is show the text on the table)=> False + ! = true
-            Assert.assertFalse(!textContainingStr.contains(str));
-        }
+        findTransactionsPage.searchResultsContainsTheGivenTextChecker(str);
     }
 
-
-    @Then("results table should show at least one result under Deposit")
-    public void results_table_should_show_at_least_one_result_under_Deposit() {
-        int sizeOfRows2 = findTransactionsPage.secondColumn.size();
-        boolean fa = false;
-        for(int i=1;i<=sizeOfRows2;i++){
-            //if it finds one under deposit the boolean becomes true and it breaks no need to continue the loop
-            // if any idea on shorting this part part or other its most welcome guys.
-            if(!findTransactionsPage.resultDeposit(i).getText().isEmpty()){
-                fa = true;
-                break;
-            }
-        }
-        Assert.assertTrue(fa);
-
-    }
-
-    @Then("results table should show at least one result under Withdrawal")
-    public void results_table_should_show_at_least_one_result_under_Withdrawal() {
-        int sizeOfRows2 = findTransactionsPage.secondColumn.size();
-        boolean fa = false;
-        for(int i=1;i<=sizeOfRows2;i++){
-            //if it finds one under deposit the boolean becomes true and it breaks no need to continue the loop
-            // if any idea on shorting this part part or other its most welcome guys.
-            if(!findTransactionsPage.resultWithdrawal(i).getText().isEmpty()){
-                fa = true;
-                break;
-            }
-        }
-        Assert.assertTrue(fa);
+    @Then("results table should show at least one result under {string}")
+    public void results_table_should_show_at_least_one_result_under(String str) {
+        findTransactionsPage.valueCheckerUnderGivenColumnName(str);
     }
 
     @When("user selects type {string}")
     public void user_selects_type(String str) {
-        Select selectedDropDown = findTransactionsPage.typeOptionsList();
-        selectedDropDown.selectByVisibleText(str);  //select from the dropdown by text
-        selectedDropDown.getFirstSelectedOption(); //the selected drop down is set as the first option to be clicked
+        findTransactionsPage.selectOption(str);
         clicks_search();   //After select type feature step, there should be "click search" step, unfortunately, it is not done,
                             //hence,the click find button method is called here, as it should be clicked otherwise it will not work
     }
 
-    @Then("results table should show no result under Withdrawal")
-    public void results_table_should_show_no_result_under_Withdrawal() {
-        int sizeOfRows2 = findTransactionsPage.secondColumn.size();
-        for(int i=1;i<=sizeOfRows2;i++){
-            //if it is empty it good according to the requirement
-            //else if it finds any in the number of rows, it will become false and fail, and
-           //Will it automatically breaks the loop if it fails to satisfy the condition?
-            Assert.assertTrue(findTransactionsPage.resultWithdrawal(i).getText().isEmpty());
-
-        }
+    @Then("results table should show no result under {string}")
+    public void results_table_should_show_no_result_under(String str) {
+        findTransactionsPage.noResultUnderGivenColumnName(str);
     }
-
-    @Then("results table should show no result under Deposit")
-    public void results_table_should_show_no_result_under_Deposit() {
-        int sizeOfRows2 = findTransactionsPage.secondColumn.size();
-        for(int i=1;i<=sizeOfRows2;i++){
-            //if it is empty it good according to the requirement
-            //else if it finds any in the number of rows, it will become false and fail, and
-            //Will it automatically breaks the loop if it fails to satisfy the condition?
-            Assert.assertTrue(findTransactionsPage.resultDeposit(i).getText().isEmpty());
-        }
-
-    }
-
 
 
 
