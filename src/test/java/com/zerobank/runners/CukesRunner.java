358dfda69c6e1cd710a7f028b4ad1852cfccd47f
yahyaayabakan <ayabakanyahya@gmail.com>
2021-08-18 18:22:37 +0100

    corrected conflict

--- src/test/java/com/zerobank/runners/CukesRunner.java
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -10,8 +10,8 @@ import org.junit.runner.RunWith;
                 "html:target/default-html-reports"},
         features= "src/test/resources/features",
         glue = "com/zerobank/stepdefinitions",
-        dryRun = true,
-        tags = "@YA"
+        dryRun = false,
+        tags = "@wip"
 
 )
 public class CukesRunner {
