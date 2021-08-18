5ea2319678657ae5f3ef5b7f81e2d05ddffbd78c
yahyaayabakan <ayabakanyahya@gmail.com>
2021-08-18 17:24:39 +0100

    corrected background last version

--- src/test/java/com/zerobank/pages/AccountSummary.java
+++ src/test/java/com/zerobank/pages/AccountSummary.java
@@ -1,9 +1,12 @@
 package com.zerobank.pages;
 
+import com.zerobank.utilities.BrowserUtils;
 import com.zerobank.utilities.Driver;
 import com.zerobank.utilities.StringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class AccountSummary extends BasePage{
 
@@ -11,6 +14,16 @@ public class AccountSummary extends BasePage{
         WebElement webElement = Driver.get().findElement(By.xpath("//a[text()='"+ new StringUtils().capitalizeWord(str)  +"']"));
         webElement.click();
 
+    }
+
+    public List<String> accountTypes(){
+        List<String> accountOptions = BrowserUtils.getElementsText(Driver.get().findElements(By.cssSelector(".board-header")));
+        return accountOptions;
+    }
+
+    public List<String> creditAccountsTableColumns(){
+        List<String> tableColumns = BrowserUtils.getElementsText(Driver.get().findElements(By.xpath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[3]/div[1]/table[1]/thead[1]/tr[1]/th")));
+        return tableColumns;
     }
 
 
--- src/test/resources/features/AccountSummary.feature
+++ src/test/resources/features/AccountSummary.feature
@@ -1,14 +1,11 @@
 Feature: Account Summary Page
+  Background:
+    Given the user is logged in
+    Given the user enters customer information
 
-  Background: Given the user is logged in
-
-  Background: And the user is on the "Account Summary" page
-
-  @seda
   Scenario: Page title should be correct
-    Then the page should have "Zero - Account Summary" title
+   Then the page should have "Zero - Account Summary" title
 
-  @seda
   Scenario: Page should have some account types
     Then the page should have following account types
       | Cash Accounts       |
@@ -16,9 +13,9 @@ Feature: Account Summary Page
       | Credit Accounts     |
       | Loan Accounts       |
 
-  @seda
+
   Scenario: Credit Accounts table columns
-    Then  "Credit Accounts" table must have following columns
+    Then  Credit Accounts table must have following columns
       | Account     |
       | Credit Card |
       | Balance     |
